48929a9982a51cfbafbb9c4b338a1d368dd6a6b9
peter-diper <rasoolfada2@gmail.com>
2025-12-05 03:22:39 +0330

    UPDATE | mainkg my code less complex

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,148 +1,51 @@
-const dailyBtn = document.querySelector(".daily");
-const weeklyBtn = document.querySelector(".weekly");
-const monthlyBtn = document.querySelector(".monthly");
-const cardTitles = document.querySelectorAll(".card--title");
-const currentTimes = document.querySelectorAll(".card--time");
-const pastTimes = document.querySelectorAll(".card--detail");
+const cards = document.querySelectorAll(".card");
+const btns = document.querySelectorAll(".set--time");
 
-//events
-dailyBtn.addEventListener("click", add);
-weeklyBtn.addEventListener("click", add);
-monthlyBtn.addEventListener("click", add);
-
-// function
-function add(e) {
-  dailyBtn.classList.remove("selected");
-  weeklyBtn.classList.remove("selected");
-  monthlyBtn.classList.remove("selected");
-  e.target.classList.add("selected");
-
-  getData();
-}
-
-//! dont look at this code XD
 async function getData() {
-  try {
-    const response = await fetch(`data.json`);
-    if (!response.ok) {
-      throw new Error(`response is not ok : ${!response.status}`);
-    }
+  const response = await fetch("data.json");
+  const result = await response.json();
 
-    const result = await response.json();
-    result.forEach((data) => {
-      if (dailyBtn.classList.contains("selected")) {
-        currentTimes.forEach((card) => {
-          const id = card.dataset.id;
-          if (id === data.title) {
-            const dataCurrent = data.timeframes.daily.current;
-            card.dataset.id === id
-              ? (card.textContent = `${String(dataCurrent)}hrs`)
-              : "";
+  cards.forEach((card) => {
+    const cardTitle = card.querySelector(".card--title");
+    const cardDateNow = card.querySelector(".card--time");
+    const cardPreviuse = card.querySelector(".card--detail");
+
+    const fetchData = result.find(
+      (data) => data.title === cardTitle.textContent
+    ).timeframes;
+    console.log(fetchData);
+
+    btns.forEach((btn) => {
+      const btnId = btn.dataset.btnValue;
+      const isSelected = btn.classList.contains("selected");
+      btn.addEventListener("click", addActive);
+      if (isSelected) {
+        cardDateNow.textContent = fetchData[btnId].current + "hr";
+
+        switch (btnId) {
+          case "daily": {
+            cardPreviuse.textContent = `Last Day ${fetchData[btnId].previous}`;
+            break;
           }
-        });
-        pastTimes.forEach((card) => {
-          const id = card.dataset.id;
-          if (id === data.title) {
-            const dataPrevious = data.timeframes.daily.previous;
-            card.dataset.id === id
-              ? (card.textContent = `Last Week ${String(dataPrevious)}hrs`)
-              : "";
+          case "weekly": {
+            cardPreviuse.textContent = `Last Week ${fetchData[btnId].previous}`;
+            break;
           }
-        });
-      }
-      if (weeklyBtn.classList.contains("selected")) {
-        currentTimes.forEach((card) => {
-          const id = card.dataset.id;
-          if (id === data.title) {
-            const dataCurrent = data.timeframes.weekly.current;
-            card.dataset.id === id
-              ? (card.textContent = `${String(dataCurrent)}hrs`)
-              : "";
+          case "monthly": {
+            cardPreviuse.textContent = `Last Month ${fetchData[btnId].previous}`;
+            break;
           }
-        });
-        pastTimes.forEach((card) => {
-          const id = card.dataset.id;
-          if (id === data.title) {
-            const dataPrevious = data.timeframes.weekly.previous;
-            card.dataset.id === id
-              ? (card.textContent = `Last Week ${String(dataPrevious)}hrs`)
-              : "";
-          }
-        });
+        }
       }
     });
-    result.forEach((data) => {
-      // daily
-      if (dailyBtn.classList.contains("selected")) {
-        currentTimes.forEach((card) => {
-          const id = card.dataset.id;
-          if (id === data.title) {
-            const dataCurrent = data.timeframes.daily.current;
-            card.dataset.id === id
-              ? (card.textContent = `${String(dataCurrent)}hrs`)
-              : "";
-          }
-        });
-        pastTimes.forEach((card) => {
-          const id = card.dataset.id;
-          if (id === data.title) {
-            const dataPrevious = data.timeframes.daily.previous;
-            card.dataset.id === id
-              ? (card.textContent = `Last Week ${String(dataPrevious)}hrs`)
-              : "";
-          }
-        });
-      }
-
-      // weekly
-      if (weeklyBtn.classList.contains("selected")) {
-        currentTimes.forEach((card) => {
-          const id = card.dataset.id;
-          if (id === data.title) {
-            const dataCurrent = data.timeframes.weekly.current;
-            card.dataset.id === id
-              ? (card.textContent = `${String(dataCurrent)}hrs`)
-              : "";
-          }
-        });
-        pastTimes.forEach((card) => {
-          const id = card.dataset.id;
-          if (id === data.title) {
-            const dataPrevious = data.timeframes.weekly.previous;
-            card.dataset.id === id
-              ? (card.textContent = `Last Week ${String(dataPrevious)}hrs`)
-              : "";
-          }
-        });
-      }
-
-      //monthly
-      if (monthlyBtn.classList.contains("selected")) {
-        currentTimes.forEach((card) => {
-          const id = card.dataset.id;
-          if (id === data.title) {
-            const dataCurrent = data.timeframes.monthly.current;
-            card.dataset.id === id
-              ? (card.textContent = `${String(dataCurrent)}hrs`)
-              : "";
-          }
-        });
-        pastTimes.forEach((card) => {
-          const id = card.dataset.id;
-          if (id === data.title) {
-            const dataPrevious = data.timeframes.monthly.previous;
-            card.dataset.id === id
-              ? (card.textContent = `Last Week ${String(dataPrevious)}hrs`)
-              : "";
-          }
-        });
-      }
-    });
-  } catch (err) {
-    console.error(err.message);
-  }
+  });
 }
+getData();
 
-document.addEventListener("DOMContentLoaded", () => {
+function addActive(e) {
+  btns.forEach((btn) => {
+    btn.classList.remove("selected");
+  });
   getData();
-});
+  e.target.classList.add("selected");
+}
